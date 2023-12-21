91437aea9f3e8d6a933e32577fd77020ad7c350d
shahzain <officeshahzain97@gmail.com>
2023-12-21 17:35:06 +0500

    Api GateWay

--- User-Service/src/main/java/com/User/Service/configuration/Configuration.java
+++ User-Service/src/main/java/com/User/Service/configuration/Configuration.java
@@ -1,11 +1,13 @@
 package com.User.Service.configuration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate()
     {
         return new RestTemplate();
